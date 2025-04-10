25a91bd0e9fb1cc75175aded15c3690bb40c9d85
Gobetti <lucas.pucci@aluno.senai.br>
2025-04-10 08:41:27 -0300

    aula5

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var numeros [5]int
+	var soma int
+
+	
+	fmt.Println("Digite 5 números inteiros:")
+
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Número %d: ", i+1)
+		fmt.Scan(&numeros[i])
+		soma += numeros[i]
+	}
+
+	
+	fmt.Printf("A soma dos números é: %d\n", soma)
+}
