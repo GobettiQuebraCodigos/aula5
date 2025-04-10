deb3c31be66648259b27ef6eb7c9e22ad5337ed7
Gobetti <lucas.pucci@aluno.senai.br>
2025-04-10 10:03:16 -0300

    idade

--- main.go
+++ main.go
@@ -1,32 +1,21 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    
 )
 
 func main() {
-	age := 16
-	fmt.Println(age <= 20)
-	fmt.Println(age >= 20)
-	fmt.Println(age == 20)
-	fmt.Println(age != 20)
-
-	if age < 30 {
-		fmt.Println("Menor que 30 anos")
-	} else {
-		fmt.Println("Maior que 30 anos")
-	}
-
-	names := []string{"Lucas", "Leonardo", "Rafael"}
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("Continue após a posição", index, "e valor", value)
-			continue
-		}
-		if index > 2 {
-			fmt.Println("Sair após", index)
-			break
-		}
-		fmt.Println("Valor: ", value)
-	}
+    var idade int
+    fmt.Println("qual sua idade?")
+    fmt.Scan(&idade)
+    if idade <18 {
+        fmt.Println("voce é menor de idade")
+    }else if idade > 18 && idade <= 60{
+        fmt.Println("voce é adulto")
+    }else if idade > 60{
+        fmt.Println("voce é idoso")
+    }
+    
+    
 }
